dcb67173be74cfd96b53c1503856960062922cbf
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2025-12-01 21:23:35 +0100

    add js

---
+++ src/main.js
@@ -0,0 +1,78 @@
+// const age = 20;
+// const username = 'Mango';
+// console.log(age);
+// console.log(username);
+
+let productName = 'Droid';
+let pricePerItem = 2000;
+productName = 'Repair droid';
+pricePerItem = 3500;
+
+console.log(productName);
+console.log(pricePerItem);
+
+// let username = 'Mango';
+// username = 'Poly';
+// console.log(username);
+
+// let username;
+// console.log(username);
+
+// username = 'Mango';
+// console.log(username);
+// const age = 20;
+// const salary = 3710.84;
+
+// console.log(age);
+// console.log(salary);
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = 'mango935';
+// const isOnline = true;
+// const isAdmin = false;
+
+// let value = null;
+// console.log(value);
+
+// let value;
+// console.log(value);
+
+// const quantity = 17;
+// console.log(typeof quantity);
+
+// const message = 'JavaScript is awesome!';
+// console.log(typeof message);
+
+// const isSidebarOpen = false;
+// console.log(typeof isSidebarOpen);
+
+// let username;
+// console.log(typeof username);
+
+// let status = null;
+// console.log(typeof status);
+
+const x = 8;
+const y = 5;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+
+// Change code below this line
+a += 2;
+b -= 4;
+c *= 3;
+d /= 10;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
