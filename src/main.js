2499463f2277ac87f9fde37fcf2c43a98df2279a
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2025-12-02 23:45:48 +0100

    study js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 // console.log(age);
 // console.log(username);
 
-let productName = 'Droid';
-let pricePerItem = 2000;
-productName = 'Repair droid';
-pricePerItem = 3500;
+// const { loadConfigFromFile } = require('vite');
 
-console.log(productName);
-console.log(pricePerItem);
+// let productName = 'Droid';
+// let pricePerItem = 2000;
+// productName = 'Repair droid';
+// pricePerItem = 3500;
+
+// console.log(productName);
+// console.log(pricePerItem);
 
 // let username = 'Mango';
 // username = 'Poly';
@@ -53,26 +55,86 @@ console.log(pricePerItem);
 // let status = null;
 // console.log(typeof status);
 
-const x = 8;
-const y = 5;
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log(x % y);
-console.log(x ** y);
+// const x = 8;
+// const y = 5;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
 
-let a = 5;
-let b = 10;
-let c = 15;
-let d = 20;
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
 
 // Change code below this line
-a += 2;
-b -= 4;
-c *= 3;
-d /= 10;
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// const age = 24;
+// const message = 'Mango ' + age + ' happy';
+// console.log(message);
+// console.log('Mango' + 55);
+// console.log('Mango' + true);
+
+// console.log(1 + '2');
+// console.log(1 + '2' + 4);
+// console.log(1 + 2 + '4');
+
+// console.log(String(5));
+// console.log(String(true));
+// console.log(String(false));
+// console.log(String(null));
+// console.log(String(undefined));
+
+// const guestName = 'Mango';
+// const roomNumber = 207;
+// const greeting = `Welcome ${guestName}, your room nymber is ${roomNumber}!`;
+// console.log(greeting);
+
+// const productName = 'Droid';
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits`;
+// console.log(message);
+
+// const productName = 'Repair droid';
+// // Якщо у змінній зберігається рядок
+// console.log(productName.length); // 12
+// // Якщо рядковий літерал
+// console.log('Repair droid'.length); // 12
+
+// let username = 'Poly';
+// const userLength = username.length;
+// const message = `Username ${username} is ${userLength} characters long`;
+// console.log(message);
+
+// const product = 'Repair droid';
+// console.log(product[0]);
+// console.log(product[5]);
+// console.log(product[11]);
+
+// const product = 'Repair droid';
+// const lastElementIndex = product.length - 1;
+// console.log(product[lastElementIndex]);
+
+// const courseTopic = 'JavaScript essentials';
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopicLength - 1];
+// console.log(courseTopicLength);
+// console.log(firstElement);
+// console.log(lastElement);
