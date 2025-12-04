4a3217439737d12f5844f3decd074cd643e7e537
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2025-12-04 22:54:29 +0100

    performent hw-1

--- src/main.js
+++ src/main.js
@@ -1,164 +1,18 @@
-// const age = 20;
-// const username = 'Mango';
-// console.log(age);
-// console.log(username);
+// function makeMessage(name, price) {
+//   //   console.log(`You picked ${name}, price per item is ${price} credits`);
+//   return `You picked ${name}, price per item is ${price} credits`;
+// }
 
-// const { loadConfigFromFile } = require('vite');
+// console.log(makeMessage('Radar', 6150));
+// // makeMessage('Reactor', 8000);
+// // makeMessage('Radar', 6150);
+// // makeMessage('Scanner', 3500);
+// // makeMessage('Engine', 4070);
 
-// let productName = 'Droid';
-// let pricePerItem = 2000;
-// productName = 'Repair droid';
-// pricePerItem = 3500;
-
-// console.log(productName);
-// console.log(pricePerItem);
-
-// let username = 'Mango';
-// username = 'Poly';
-// console.log(username);
-
-// let username;
-// console.log(username);
-
-// username = 'Mango';
-// console.log(username);
-// const age = 20;
-// const salary = 3710.84;
-
-// console.log(age);
-// console.log(salary);
-
-// const topSpeed = 160;
-// const distance = 617.54;
-// const login = 'mango935';
-// const isOnline = true;
-// const isAdmin = false;
-
-// let value = null;
-// console.log(value);
-
-// let value;
-// console.log(value);
-
-// const quantity = 17;
-// console.log(typeof quantity);
-
-// const message = 'JavaScript is awesome!';
-// console.log(typeof message);
-
-// const isSidebarOpen = false;
-// console.log(typeof isSidebarOpen);
-
-// let username;
-// console.log(typeof username);
-
-// let status = null;
-// console.log(typeof status);
-
-// const x = 8;
-// const y = 5;
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x * y);
-// console.log(x / y);
-// console.log(x % y);
-// console.log(x ** y);
-
-// let a = 5;
-// let b = 10;
-// let c = 15;
-// let d = 20;
-
-// Change code below this line
-// a += 2;
-// b -= 4;
-// c *= 3;
-// d /= 10;
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// console.log(d);
-// const age = 24;
-// const message = 'Mango ' + age + ' happy';
-// console.log(message);
-// console.log('Mango' + 55);
-// console.log('Mango' + true);
-
-// console.log(1 + '2');
-// console.log(1 + '2' + 4);
-// console.log(1 + 2 + '4');
-
-// console.log(String(5));
-// console.log(String(true));
-// console.log(String(false));
-// console.log(String(null));
-// console.log(String(undefined));
-
-// const guestName = 'Mango';
-// const roomNumber = 207;
-// const greeting = `Welcome ${guestName}, your room nymber is ${roomNumber}!`;
-// console.log(greeting);
-
-// const productName = 'Droid';
-// const pricePerItem = 3500;
-// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
-// console.log(message);
-
-// const pricePerDroid = 800;
-// const orderedQuantity = 6;
-// const deliveryFee = 50;
-// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
-// const message = `You ordered droids worth ${totalPrice} credits`;
-// console.log(message);
-
-// const productName = 'Repair droid';
-// // Якщо у змінній зберігається рядок
-// console.log(productName.length); // 12
-// // Якщо рядковий літерал
-// console.log('Repair droid'.length); // 12
-
-// let username = 'Poly';
-// const userLength = username.length;
-// const message = `Username ${username} is ${userLength} characters long`;
-// console.log(message);
-
-// const product = 'Repair droid';
-// console.log(product[0]);
-// console.log(product[5]);
-// console.log(product[11]);
-
-// const product = 'Repair droid';
-// const lastElementIndex = product.length - 1;
-// console.log(product[lastElementIndex]);
-
-// const courseTopic = 'JavaScript essentials';
-// const courseTopicLength = courseTopic.length;
-// const firstElement = courseTopic[0];
-// const lastElement = courseTopic[courseTopicLength - 1];
-// console.log(courseTopicLength);
-// console.log(firstElement);
-// console.log(lastElement);
-
-// const a = 2;
-// const b = 5;
-// console.log(a > b); // false
-// console.log(b > a); // true
-// console.log(a >= b); // false
-// console.log(b >= a); // true
-// console.log(a < b); // true
-// console.log(b < a); // false
-// console.log(a <= b); // true
-// console.log(b <= a); // false
-
-// const age = 16;
-// const isAdult = age >= 18;
-// console.log(isAdult);
-
-// console.log(5 == 5); // true
-// console.log(5 == 3); // false
-// console.log(5 != 3); // true
-// console.log(5 != 5); // false
-
-const correctPassword = 'jqueryismyjam';
-const userPassword = 'mangodab3st';
-const isValid = correctPassword === userPassword;
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
